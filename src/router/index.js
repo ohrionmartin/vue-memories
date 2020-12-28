ac2b91bd73d2448cfbf86cebca77ca9e456b382e
ohrionmartin <oh.martin@vdm.io>
2020-12-28 13:13:03 +0200

    Removed Views folder and route to Home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
 ]
 
 const router = createRouter({
