5723de97dce2f32149a2635dc8a637a9c2dea804
ohrionmartin <oh.martin@vdm.io>
2020-12-29 10:40:38 +0200

    Added required mutations, actions in the store and also methods to add/create memories

--- src/components/memories/CreateMemoryForm.vue
+++ src/components/memories/CreateMemoryForm.vue
@@ -1,20 +1,20 @@
 <template>
-    <form class="ion-padding">
+    <form class="ion-padding" @submit.prevent="submitForm">
         <ion-list>
             <ion-item>
                 <ion-label position="floating">Title</ion-label>
-                <ion-input type="text" required/>
+                <ion-input type="text" required v-model="enteredTitle"/>
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Image URL</ion-label>
-                <ion-input type="url" required/>
+                <ion-input type="url" required v-model="enteredImageUrl"/>
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Description</ion-label>
-                <ion-textarea rows="5"></ion-textarea>
+                <ion-textarea rows="5" v-model="enteredDescription"></ion-textarea>
             </ion-item>
         </ion-list>
-            <ion-button expand="block">Save</ion-button>
+            <ion-button type="submit" expand="block">Save</ion-button>
     </form>
 </template>
 
@@ -22,14 +22,32 @@
 import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from '@ionic/vue';
 
 export default {
-  components: {
-    IonList,
-    IonItem,
-    IonLabel,
-    IonInput,
-    IonTextarea,
-    IonButton
-  },
+    emits: ['save-memory'],
+    components: {
+        IonList,
+        IonItem,
+        IonLabel,
+        IonInput,
+        IonTextarea,
+        IonButton
+    },
+    data () {
+        return {
+            enteredTitle: '',
+            enteredImage: '',
+            enteredDescription: ''
+        };
+    },
+    methods: {
+        submitForm() {
+            const memoryData = {
+                title: this.enteredTitle,
+                imageUrl: this.enteredImageUrl,
+                description: this.enteredDescription
+            };
+            this.$emit('save-memory', memoryData);
+        }
+    }
     
 };
 </script>
